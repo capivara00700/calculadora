c42ed0eb286038cb685800735d724aceef158b34
Capivara <danilo.duarte00700@gmail.com>
2025-02-04 17:50:08 -0200

    .

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function numero(num){
-
     let numero = document.getElementById('resultados').value
     document.getElementById('resultados').value = numero + num
-    
+}
+function clean(){
+    document.getElementById('resultados').value = null
+}
+function calcular(){
+    let numero = document.getElementById('resultados').value
+    document.getElementById('resultados').value = eval(numero)
+}
+function voltar(){
+    let resultado = document.getElementById('resultados').value
+    document.getElementById('resultados').value = resultado.substring(0, resultado.length -1)
 }
