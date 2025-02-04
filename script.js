30d178825aef6643c3bb4f5cebefb3bcf557b66a
Capivara <danilo.duarte00700@gmail.com>
2025-02-04 16:42:44 -0200

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+function calcular(tipo){
+
+    if(!isNaN(tipo)){
+        console.log('numero')
+        console.log(tipo)
+    } else {
+        console.log('sinal')
+        console.log(tipo)
+
+    }
+
+}
